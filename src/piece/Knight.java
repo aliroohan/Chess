d42d81fa54b582f6273c21eee55630289768c497
aliroohan <aliroohan321@gmail.com>
2024-04-19 10:55:28 +0500

    Basic implementation of KNIGHT piece Added

--- src/piece/Knight.java
+++ src/piece/Knight.java
@@ -11,4 +11,16 @@ public class Knight extends Piece{
             image = getImage("/piece/b-knight");
         }
     }
+
+    @Override
+    public boolean canMove(int targetCol, int targetRow) {
+        if (isWithinBounds(targetCol, targetRow)) {
+            if (Math.abs(targetCol - prevCol) == 2 && Math.abs(targetRow - prevRow) == 1) {
+                return isValidSquare(targetCol, targetRow);
+            } else if (Math.abs(targetCol - prevCol) == 1 && Math.abs(targetRow - prevRow) == 2) {
+                return isValidSquare(targetCol, targetRow);
+            }
+        }
+        return false;
+    }
 }
